e5b2fb5284e7ec7865420d372268148094b6612a
Tyler Robinson <tylerrhb11@gmail.com>
2022-05-23 20:23:51 -0400

    editing course saves locally, doesn't PUT to backend

--- src/components/containers/EditCourseContainer.js
+++ src/components/containers/EditCourseContainer.js
@@ -1,18 +1,67 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import { Redirect } from 'react-router-dom';
 import { fetchCourseThunk, editCourseThunk } from "../../store/thunks";
 import { EditCourseView } from "../views";
 
 class EditCourseContainer extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      title: "", 
+      timeslot: "",
+      location: "", 
+      instructorId: null, 
+      redirect: false, 
+      redirectId: null
+    };
+}
     componentDidMount() {
         console.log(this.props);
         //getting course ID from url
         this.props.fetchCourse(this.props.match.params.id);
     }
 
+    handleChange = event => {
+      this.setState({
+        [event.target.name]: event.target.value
+      });
+    }
+
+    handleSubmit = async event => {
+      event.preventDefault();
+      let course = {
+        title: this.state.title,
+        tiemeslot: this.state.timeslot,
+        location: this.state.location,
+        instructorId: this.state.instructorId
+      }
+      this.props.course.title = this.state.title
+      this.props.course.timeslot = this.state.timeslot;
+      this.props.course.location = this.state.location;
+      this.props.course.instructorId = this.state.instructorId;
+
+      let updatedCourse = await this.props.editCourse(this.props.course);
+      
+      this.setState({
+        title: course.title,
+        timeslot: course.timeslot,
+        location: course.location,
+        instructorId: course.instructorId,
+        redirect: true, 
+        redirectId: course.id
+      });
+    }
+
     render () {
+      if(this.state.redirect) {
+        return (<Redirect to={`/course/${this.state.redirectId}`}/>)
+      }
         return (
+          
             <EditCourseView
+                handleChange = {this.handleChange} 
+                handleSubmit={this.handleSubmit} 
                 course={this.props.course}
                 editCourse={this.props.editCourse}
             />
--- src/components/views/EditCourseView.js
+++ src/components/views/EditCourseView.js
@@ -1,24 +1,42 @@
 import { Link } from "react-router-dom";
 
 const EditCourseView = (props) => {
+    const {handleChange, handleSubmit } = props;
     let { course, editCourse } =  props;
     return (
-        <div>
-            <p>Update Course Information for {course.title}:</p>
-            <form onSubmit={editCourse(course)}>
-                <p>Title</p> 
-                <input type="text" name="title"/>
-                <p>Timeslot</p>
-                <input type="text" name="timeslot"/>
-                <p>Location</p>
-                <input type="text" name="location"/>
-                <p>Instructor ID</p>
-                <input type="number" name="instructorId"/>
-                <button type="submit">Save</button>
-            </form>
-            <Link to={`/course/${course.id}`}>
-                <p>Back</p>
-            </Link>
+        <div className="root">
+        <div className="formContainer">
+          <div className="formTitle">
+            <h2 style={{fontWeight: 'bold', fontFamily: 'Courier, sans-serif', fontSize: '20px', color: '#11153e'}}>
+              Update Information
+            </h2>
+          </div>
+          <form style={{textAlign: 'center'}} onSubmit={(e) => handleSubmit(e)}>
+            <label style= {{color:'#11153e', fontWeight: 'bold'}}>Title: </label>
+            <input type="text" name="title" onChange ={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+  
+            <label style={{color:'#11153e', fontWeight: 'bold'}}>Timeslot: </label>
+            <input type="text" name="timeslot" onChange={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+  
+            <label style={{color:'#11153e', fontWeight: 'bold'}}>Location: </label>
+            <input type="text" name="location" onChange={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+  
+            <label style={{color:'#11153e', fontWeight: 'bold'}}>instructorId: </label>
+            <input type="text" name="instructorId" onChange={(e) => handleChange(e)} />
+            <br/>
+            <br/>
+            
+            <button type="submit" onClick={() => editCourse(course)}>Submit</button>
+            <br/>
+            <br/>
+          </form>
+          </div>
         </div>
     );
 }
